e6400dff6c92d0970fc39fba5a6d5455deb60c1b
i0n <ianalexanderwood@gmail.com>
2014-07-29 06:43:28 -0700

    version 0.0.4

--- main.go
+++ main.go
@@ -10,35 +10,42 @@ import(
 )
 
 func main() {
-    done := make(chan bool)
-    events := make(chan supervisor.Event)
-    evl := supervisor.NewListener(os.Stdin, os.Stdout)
-    sigs := make(chan os.Signal, 1)
 
-    signal.Notify(sigs, syscall.SIGTERM)
+  done := make(chan bool)
+  events := make(chan supervisor.Event)
+  evl := supervisor.NewListener(os.Stdin, os.Stdout)
+  signals := make(chan os.Signal, 1)
+  signal.Notify(signals, syscall.SIGTERM)
+  args := os.Args[1:]
 
-    go func() {
-        fmt.Fprintf(os.Stderr, "SIGTERM received\n")
-        time.Sleep(3 * time.Second)
-        syscall.Kill(-1, syscall.SIGKILL)
-        done <- true
-    }()
+  for _, arg := range args {
+    if arg == "-v" || arg == "--version" {
+      fmt.Fprintf(os.Stdout, "%s\n", Version)
+      return
+    }
+  }
 
-    go func() {
-      for event := range events {
-        fmt.Fprintf(os.Stderr, "Got event: %s\n", event)
-          if event.Name() == "PROCESS_STATE_FATAL" {
-            fmt.Fprintf(os.Stderr, "Killing!\n")
-              // Calling -1 (stop everything) here as you cannot stop pid 1 by calling '1'
-              syscall.Kill(-1, syscall.SIGTERM)
-          } else {
-            fmt.Fprintf(os.Stderr, "Non Fatal!\n")
-          }
+  go func() {
+    for signal := range signals {
+      _ = signal
+      fmt.Fprintf(os.Stderr, "SIGTERM received\n")
+      time.Sleep(3 * time.Second)
+      syscall.Kill(-1, syscall.SIGKILL)
+    }
+    done <- true
+  }()
+
+  go func() {
+    for event := range events {
+      if event.Name() == "PROCESS_STATE_FATAL" {
+        // Calling -1 (stop everything) here as you cannot stop pid 1 by calling '1'
+        syscall.Kill(-1, syscall.SIGTERM)
       }
-      done <- true
-    }()
+    }
+    done <- true
+  }()
 
-    evl.Run(events)
-    close(events)
-    <-done
+  evl.Run(events)
+  close(events)
+  <-done
 }
